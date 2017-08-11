ac0c98202f489b3b9640498ec5a07c39c75e3c4c
Syafiq Kamarul Azman <syafiqkamarulazman@gmail.com>
2017-08-12 00:35:34 +0400

    Added logging into load_obama_vec.py

--- load_obama_vec.py
+++ load_obama_vec.py
@@ -1,6 +1,8 @@
 import logging
 from gensim.models import KeyedVectors
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.INFO)
 obama_vec = KeyedVectors.load_word2vec_format("obama_vec.bin", binary=True)
 
 print("Testing model...")
